c22521b4e43378fc2da704209c4a9dcea7c5f913
Lukas Heddendorp <lu.heddendorp@gmail.com>
2016-02-25 21:24:41 +0100

    Also report the name

--- js/app.js
+++ js/app.js
@@ -38,7 +38,7 @@ app.controller('AppController', function($http, $mdToast, $log, $analytics) {
                 vm.changes = generate(build1, build2);
             request = true;
         });
-        $analytics.eventTrack(vm.build1+' - '+vm.build2, {  category: 'Changelog display', label: vm.data.display_name });
+        $analytics.eventTrack(vm.build1+' - '+vm.build2+'  of '+vm.data.display_name, {  category: 'Changelog display'});
     };
 
     function generate (b1, b2){
