a51eb82e74cc9df147ea7eeec964b1599d508486
Lukas Heddendorp <lu.heddendorp@gmail.com>
2016-02-26 15:17:38 +0100

    Better version sorting

--- js/app.js
+++ js/app.js
@@ -22,7 +22,10 @@ app.controller('AppController', function($http, $mdToast, $log, $analytics) {
           $log.info('Solder api loaded');
           $log.info(res.data);
           vm.data = res.data;
-          vm.data.builds.reverse();
+          vm.data.builds = vm.data.builds.map(function (build) {
+            return build.replace(/([a-z])|([A-Z])/g, '');
+          });
+          vm.data.builds.sort(compareVersion).reverse();
           $analytics.eventTrack('loaded: '+res.data.name, {  category: 'Pack loaded', label: res.data.display_name });
           $mdToast.showSimple('Pack data loaded');
         }, function (err) {
@@ -47,6 +50,11 @@ app.controller('AppController', function($http, $mdToast, $log, $analytics) {
   vm.update = function () {
     if (!vm.build1 || !vm.build2){
       return;
+    }
+    if(compareVersion (vm.build1, vm.build2) > 0){
+      var temp = vm.build1;
+      vm.build1 = vm.build2;
+      vm.build2 = temp;
     }
     vm.progress = 0;
     $log.info('Loading builds '+vm.build1.replace(/ /g,'')+' & '+vm.build2.replace(/ /g,''));
@@ -93,5 +101,19 @@ app.controller('AppController', function($http, $mdToast, $log, $analytics) {
         changes.removes.push(mod.pretty_name);
     });
     return changes;
+  }
+
+  function compareVersion (a, b) {
+    var i, cmp, len, re = /(\.0)+[^\.]*$/;
+    a = (a + '').replace(re, '').split('.');
+    b = (b + '').replace(re, '').split('.');
+    len = Math.min(a.length, b.length);
+    for( i = 0; i < len; i++ ) {
+      cmp = parseInt(a[i], 10) - parseInt(b[i], 10);
+      if( cmp !== 0 ) {
+        return cmp;
+      }
+    }
+    return a.length - b.length;
   }
 });
